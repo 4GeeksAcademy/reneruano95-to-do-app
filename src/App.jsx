99986b47cdf9d2191e38b72b846280fee208a98d
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-02-23 13:54:44 +0000

    first setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+const App = () => {
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working...
 			</a>
@@ -23,4 +16,4 @@ const Home = () => {
 	);
 };
 
-export default Home;
+export default App;
